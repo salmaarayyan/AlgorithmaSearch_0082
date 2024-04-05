81a90aee2cbe78b63b30337e566676611b0df138
salmaarayyan <salmaarayyan@gmail.com>
2024-04-05 14:46:26 +0800

    membuat looping for

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -21,4 +21,9 @@ void input()
     cout << "\n---------------------\n";
     cout << " enter array elements \n";
     cout << "----------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << "> ";
+        
+    }
 }
