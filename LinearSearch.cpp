8542569a322b01041e55e604ff4ac32e8d837b8d
salmaarayyan <salmaarayyan@gmail.com>
2024-04-05 14:59:59 +0800

    membuat step 4

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -48,7 +48,9 @@ void LinearSearch()
             if (arr[i] == item)
             {
                 cout << "\n" << item << " found at position " << (i + 1) << endl;  //step 6
+                break;
             }
+            i++;  //step 4
         }
     } while
     
