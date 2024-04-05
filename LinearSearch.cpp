873b2f4d46d9d3533c62eec5e1ca6148a07fca22
salmaarayyan <salmaarayyan@gmail.com>
2024-04-05 14:43:19 +0800

    membuat perkondisian if else

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -11,5 +11,9 @@ void input()
     {
         cout << "enter the number of elements in the array : ";
         cin >> n;
+        if ((n>0) && (n <= 20))
+            break;
+        else
+            cout << "\narray should have minimum 1 and maximum 20 elements. \n\n";
     }
 }
