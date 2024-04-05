a1c8c2d6ca8b47b49ccf0b4ef9c4bccd5ec386d3
salmaarayyan <salmaarayyan@gmail.com>
2024-04-05 14:53:41 +0800

    membuat step 2

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -41,6 +41,9 @@ void LinearSearch()
         //accept the number to be searched
         cout << "\nenter the element you want to search : ";  //step 1
         cin >> item;
+
+        ctr = 0;
+        i = 0;  //step 2
     });
     
 }
